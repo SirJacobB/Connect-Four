d7425bf850780ad2471f2dce74bc2160940afd1b
sirJacobB <jacob.brillante.te15d@gmail.com>
2018-04-20 23:01:22 +0200

    Further improved visibility

--- src/Client/Column.java
+++ src/Client/Column.java
@@ -2,17 +2,19 @@ package Client;
 
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.shape.StrokeType;
 
 public class Column extends Group{
 	
 	private Rectangle r;
+	private Circle bgCircle;
 	
 	public Column(int i) {
 		createRectangle(i);
 		setColour();
 		columnInteraction(i);
+		createBgCircles();
 	}
 	
 	private void createRectangle(int cr) {
@@ -21,15 +23,24 @@ public class Column extends Group{
 	}
 	
 	private void setColour() {
-		r.setFill(Color.rgb(0, 0, 255, 0.2));
-		r.setStroke(Color.rgb(15, 120, 255));
-		r.setStrokeWidth(5.0);
-		r.setStrokeType(StrokeType.OUTSIDE);
+		r.setFill(Color.rgb(92, 189, 234));
 	}
 	
-	private void columnInteraction(int coli) {
+	private void columnInteraction(int colu) {
 		r.setOnMouseClicked(event -> {
-			Game.spawnDisc(coli);
+			Game.spawnDisc(colu);
 		});
+	}
+	
+	private void createBgCircles() {
+		for(int i = 0; i < 7; i++) {
+			for(int j = 0; j < 6; j++){
+				bgCircle = new Circle(50);
+				bgCircle.setFill(Color.rgb(227, 242, 253));
+				this.getChildren().add(bgCircle);
+				bgCircle.setTranslateX(i*Game.WORLD_WIDTH/7 + Game.WORLD_WIDTH/14);
+				bgCircle.setTranslateY(j*Game.WORLD_HEIGHT/6 + bgCircle.getRadius() + bgCircle.getRadius()/3);
+			}
+		}
 	}
 }
